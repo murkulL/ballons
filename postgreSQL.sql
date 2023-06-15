2dc7f2bcc1d54887f700f78aed7c2ae42c996c47
Oleg Shutka <olegov23345@gmail.com>
2023-06-15 18:14:31 +0300

    fix

--- postgreSQL.sql
+++ postgreSQL.sql
@@ -103,3 +103,75 @@ WHERE EXISTS (
 -- SELECT 1 возвращает только одно значение "1" для каждой 
 -- строки, в то время как SELECT * вернул бы все столбцы таблицы
 -- "orders", что может потребовать больше ресурсов для выполнения запроса.
+
+
+
+--выбрать продукты которые не покупались с периода 95-02-1 по 95-02-15
+
+SELECT product_name 
+FROM products
+WHERE NOT EXISTS(SELECT orders.order_id
+				FROM orders
+				INNER JOIN order_details USING(order_id) 
+				WHERE order_details.product_id = products.product_id 
+				AND order_date BETWEEN '1995-02-01' AND '1995-02-15'
+ ) 
+
+
+
+
+-------------------------------------------------------------
+
+SELECT company_name, country
+FROM suppliers
+WHERE country IN (SELECT DISTINCT country
+FROM customers )
+
+--РАВНЫЕ ПО ЗНАЧЕНИЮ 
+
+SELECT DISTINCT suppliers.company_name
+FROM suppliers
+JOIN customers USING(country)
+
+--------------------------------------------------------
+В SQL ключевое слово "ANY" используется в операторе сравнения для выполнения сравнения с любым значением из набора значений. Оно может быть использовано с операторами сравнения, такими как "=", ">", "<", ">=", "<=", "<>", "IN" и "LIKE".
+
+Вот несколько примеров использования ключевого слова "ANY" в SQL:
+
+Использование оператора сравнения "=" с ключевым словом "ANY":
+
+SELECT * FROM employees WHERE age = ANY (25, 30, 35);
+Этот запрос вернет все записи из таблицы "employees", где значение столбца "age" равно любому из значений 25, 30 или 35.
+
+---
+
+
+Использование оператора сравнения ">" с ключевым словом "ANY":
+
+SELECT * FROM products WHERE price > ANY (SELECT price FROM products WHERE category = 'Electronics');
+
+В этом примере будет выбраны все записи из таблицы 
+"products", где цена продукта больше любой цены
+ продукта в категории "Electronics"
+
+
+----
+Использование оператора "IN" с ключевым словом "ANY":
+
+SELECT * FROM orders WHERE status IN (ANY ('Shipped', 'Delivered'));
+
+
+Этот запрос вернет все записи из таблицы "orders", где статус заказа равен "Shipped" или "Delivered".
+
+
+
+
+--ВЫБРАТЬ ТАКИЕ ПРОДУКТЫ КОЛИЧЕСТВО КОТОРЫХ БОЛЬШЕ СРЕДНЕГО ПО ЗАКАЗАМ
+
+SELECT DISTINCT product_name, quantity
+FROM products
+JOIN order_details USING(product_id)
+WHERE quantity > ( SELECT AVG(quantity)
+				   FROM order_details)
+ORDER BY quantity DESC
+
